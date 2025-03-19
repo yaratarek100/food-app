39ab87427bbac95a1bf05161ad21bc00649e7886
yaratarek100 <yara.tarek.300@gmail.com>
2025-03-19 19:39:14 +0200

    background

--- src/modules/Shared/Authlayout/Authlayout.jsx
+++ src/modules/Shared/Authlayout/Authlayout.jsx
@@ -13,13 +13,13 @@ useEffect(()=>{
   if(window.location.pathname==("/signup")){
     setIsRegister(true);
   }
-},[])
+},[,window.location.pathname])
 
   return (
     <div className={`${isRegister? "register" : ""} auth-container `}>
       <div className=" container-fluid bg-layer h-100">
         <div className="row w-100 h-100 justify-content-center align-items-center">
-          <div className= {`${isRegister? "col-md-7 ":"col-md-5"}  bg-white rounded-5 text-center p-3 mt-5`}>
+          <div className= {`${isRegister? "col-md-7":"col-md-5"}  bg-white  rounded-5 text-center p-3 mt-5`}>
             <div className="logo w-50 mx-auto">
               <img src={logo} alt="" className="w-100" />
             </div>
