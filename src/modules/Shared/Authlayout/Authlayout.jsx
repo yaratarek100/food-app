4bbf785dbf17aa1706931fd4c248353fc80ec8e6
yaratarek100 <yara.tarek.300@gmail.com>
2025-03-19 19:48:04 +0200

    bg

--- src/modules/Shared/Authlayout/Authlayout.jsx
+++ src/modules/Shared/Authlayout/Authlayout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import logo from "../../../assets/4 4.png";
 
 
@@ -8,12 +8,11 @@ import logo from "../../../assets/4 4.png";
 
 export default function Authlayout() {
   const [isRegister, setIsRegister] = useState(false)
+  const location = useLocation().pathname;
 
-useEffect(()=>{
-  if(window.location.pathname==("/signup")){
-    setIsRegister(true);
-  }
-},[,window.location.pathname])
+  useEffect(() => {
+    setIsRegister(location === "/signup");
+  }, [location]);
 
   return (
     <div className={`${isRegister? "register" : ""} auth-container `}>
